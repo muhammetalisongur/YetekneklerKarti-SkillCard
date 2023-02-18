d0c636c9961a8313359e4a117a808b7c711a9161
Muhammet Ali Songur <muhammetalisongur@gmail.com>
2023-02-18 06:07:19 +0300

    Update

---
+++ DataAccessLayer/Migrations/202302180301408_deleteadmin.cs
@@ -0,0 +1,17 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class deleteadmin : DbMigration
+    {
+        public override void Up()
+        {
+            DropTable("dbo.Admins");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
