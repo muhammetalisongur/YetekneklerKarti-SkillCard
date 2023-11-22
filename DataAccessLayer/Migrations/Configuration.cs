a9307064082850e525b3b72a753d9aa0c16fac29
Muhammet Ali Songur <muhammetalisongur@hotmail.com>
2023-11-22 10:49:27 +0300

    #

--- DataAccessLayer/Migrations/Configuration.cs
+++ DataAccessLayer/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 ﻿namespace DataAccessLayer.Migrations
 {
+    using EntityLayer.Concrete;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,10 +15,50 @@
 
         protected override void Seed(DataAccessLayer.Concrete.Context context)
         {
-            //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+            context.Profils.AddOrUpdate(x => x.ProfilID,
+                new EntityLayer.Concrete.Profil()
+                {
+                    UserName = "Muhammet Ali",
+                    SurName = "Songur",
+                    Message = "“Tereddüt edersen bacakların seni taşımaz. Yürüyeceğim de, bas ve yürü!”",
+                    Image = "/Images/MAS.jpg"
+                });
+
+            context.Skills.AddOrUpdate(x => x.SkillID,
+            new EntityLayer.Concrete.Skill()
+            {
+                SkillName = "C#",
+                SkillValue = 50,
+            },
+            new Skill()
+            {
+                SkillName = "HTML",
+                SkillValue = 100,
+            },
+            new Skill()
+            {
+                SkillName = "CSS",
+                SkillValue = 70,
+            },
+            new Skill()
+            {
+                SkillName = "Javascript",
+                SkillValue = 60,
+            },
+            new Skill()
+            {
+                SkillName = "Jquery",
+                SkillValue = 50,
+            },
+            new Skill()
+            {
+                SkillName = "Bootstrap",
+                SkillValue = 80,
+            });
+
+
+
         }
     }
 }
